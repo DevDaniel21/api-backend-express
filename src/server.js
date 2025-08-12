6ae8dda83d8446e6ed574b0b0834a990169b822e
DevDaniel21 <danielglaucia2.00@gmail.com>
2025-08-12 17:49:18 -0300

    Criado o crud do profile

--- src/server.js
+++ src/server.js
@@ -3,10 +3,34 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json())
+
+// CRUD profile
+app.post('/profile', (req, res) => {
+  const dados = req.body
+  res.json({
+    message: "Profile has created successfully!",
+    profile: dados
+  })
+})
+
+app.get('/profile', (req, res) => {
+  res.json({message: "Data of profile has been taken successfully!"})
+})
+
+app.put('/profile', (req, res) => {
+  const dados = req.body
+  res.json({
+    message: "Profile has updated successfully!",
+    profile: dados
+  })
+})
+
+app.delete('/profile', (req, res) => {
+  res.json({message: "Profile has deleted successfully!"})
 })
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`)
+  console.log('Use Ctrl+C para desligar o servidor')
 })
